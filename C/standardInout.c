7e89bfde800630d46a455d1bb1419d719be48e76
Trieu Than <trieuthan92pro@gmail.com>
2021-10-09 12:13:27 +0700

    Update ex 201

---
+++ C/standardInout.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	
+//	int c;
+//	c= getchar();
+//	printf("Lan 1: %c\nLan 2: ", c);
+//	int cc = putchar( c );
+//	printf("\nLan 3: %c", cc);
+	char str[15];
+	fgets(str, 15, stdin );
+	puts(str);
+	
+	return 0;
+}
+
