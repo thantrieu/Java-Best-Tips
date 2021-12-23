a0b2b05c7242445d5f5dd9d0a1e9c60a63485ab6
Trieu Than <trieuthan92pro@gmail.com>
2021-12-23 09:04:13 +0700

    Add new feature in java17

---
+++ Java/JEP406.java
@@ -0,0 +1,34 @@
+/**
+ * Pattern matching for switch(preview in Java 17)
+ */
+public class JEP406 {
+    public static void main(String[] args) {
+        System.out.println(formatterJava17(120L));
+        System.out.println(formatterJava17("Branium Academy"));
+        System.out.println(formatterJava17(3.25));
+    }
+
+    private static String formatter(Object o) {
+        String formatted = "unknown";
+        if (o instanceof Integer i) {
+            formatted = String.format("int %d", i);
+        } else if (o instanceof Long l) {
+            formatted = String.format("long %d", l);
+        } else if (o instanceof Double d) {
+            formatted = String.format("double %f", d);
+        } else if (o instanceof String s) {
+            formatted = String.format("String %s", s);
+        }
+        return formatted;
+    }
+
+    private static String formatterJava17(Object o) {
+        return switch (o) {
+            case Integer i -> String.format("int %d", i);
+            case Long l    -> String.format("long %d", l);
+            case Double d  -> String.format("double %f", d);
+            case String s  -> String.format("String %s", s);
+            default        -> o.toString();
+        };
+    }
+}
